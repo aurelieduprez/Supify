4e127d06cd5dd12e867ef7769b00d77c569f668b
aurelieduprez <nelphae@gmail.com>
2021-01-26 21:39:27 +0100

    pause button !!!

--- Supify/wwwroot/js/site.js
+++ Supify/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var wavesurfer = WaveSurfer.create({
+    container: '#waveform'
+});
+wavesurfer.load('/music/theWeekend.mp3');
+wavesurfer.on('ready', function () {
+    wavesurfer.setProgressColor("#415a77");
+    wavesurfer.setWaveColor("#778da9");
+    wavesurfer.play();
+    wavesurfer.setVolume(0.1);
+});
