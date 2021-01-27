414995a2ec14fa45f307a4bf2bc544a149d3de8d
aurelieduprez <nelphae@gmail.com>
2021-01-27 11:52:25 +0100

    volume manager

--- Supify/wwwroot/js/site.js
+++ Supify/wwwroot/js/site.js
@@ -3,14 +3,30 @@
 
 // Write your JavaScript code.
 
-
+//waveform
 var wavesurfer = WaveSurfer.create({
-    container: '#waveform'
+    container: '#waveform',
+    barWidth: 5,
+    barRadius: 5,
+    cursorWidth: 0,
+    height: 200,
+    hideScrollbar: true,
+    partialRender: true
 });
-wavesurfer.load('/music/theWeekend.mp3');
+//load music
+wavesurfer.load('/music/onlyYou.mp3');
 wavesurfer.on('ready', function () {
     wavesurfer.setProgressColor("#415a77");
     wavesurfer.setWaveColor("#778da9");
     wavesurfer.play();
-    wavesurfer.setVolume(0.1);
+    wavesurfer.setVolume(0.2);
 });
+
+// volume cursor
+document.querySelector('#volume').value = wavesurfer.backend.getVolume();
+var volumeInput = document.querySelector('#volume');
+var onChangeVolume = function (e) {
+    wavesurfer.setVolume(e.target.value);
+    console.log(e.target.value);
+};
+volumeInput.addEventListener('input', onChangeVolume);
